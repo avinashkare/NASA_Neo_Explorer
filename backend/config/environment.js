42062ff033e3b55a297c73acfa351b2631d6a728
Avinash Kare <pen.killer77@gmail.com>
2025-06-28 02:17:41 +0100

    code refactored

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,14 +1,7 @@
 require('dotenv').config();
 
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: process.env.DB_USER,         
-  host: process.env.DB_HOST,         
-  database: process.env.DB_DATABASE, 
-  password: process.env.DB_PASSWORD, 
-  port: process.env.DB_PORT,         
-});
-
-module.exports = pool;
-
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  NASA_API_KEY: process.env.NASA_API_KEY,
+  NASA_BASE_URL: process.env.NASA_BASE_URL
+};
